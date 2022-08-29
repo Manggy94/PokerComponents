9d8c59f1197e7a82e38c36df03d32e67355fd0f5
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-08-29 16:44:27 +0200

    Updating Tournaments, Levels and associated tests

--- Tests/components/tournament/test_level.py
+++ Tests/components/tournament/test_level.py
@@ -1,8 +1,37 @@
 import unittest
+import components.tournament as tournament
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class MyLevelTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.level = tournament.Level(8, 1600)
+
+    def test_new_level(self):
+        self.assertIsInstance(self.level, tournament.Level)
+        self.assertIsInstance(self.level.level, int)
+        self.assertIsInstance(self.level._sb, float)
+        self.assertIsInstance(self.level._bb, float)
+        self.assertIsInstance(self.level._ante, float)
+
+    def test_bb(self):
+        with self.assertRaises(ValueError):
+            self.level.bb = -1
+        self.assertEqual(self.level.bb, 1600)
+        self.assertEqual(self.level.sb, 800)
+
+    def test_ante(self):
+        with self.assertRaises(ValueError):
+            self.level.ante = -1
+        self.assertEqual(self.level.ante, 200)
+
+    def test_level(self):
+        with self.assertRaises(ValueError):
+            self.level.level = -2
+        with self.assertRaises(ValueError):
+            self.level.level = 0.1
+        self.assertEqual(self.level.level, 8)
+
 
 if __name__ == '__main__':
     unittest.main()
--- Tests/components/tournament/test_tournament.py
+++ Tests/components/tournament/test_tournament.py
@@ -1,8 +1,18 @@
 import unittest
+import components.tournament as tournament
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+
+class MyTournamentTestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.tour = tournament.Tournament("tour_id", name="PLD", money_type="real")
+
+    def test_new_tournament(self):
+        tour = tournament.Tournament()
+        self.assertIsInstance(tour, tournament.Tournament)
+        self.assertEqual(tour.id, "0000")
+        self.assertEqual(tour.name, "Kill The Fish")
+
 
 if __name__ == '__main__':
     unittest.main()
