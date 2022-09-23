54c3f7d4b6ce7522a3a8fcdcf9feb34825e3bcc6
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-09-23 18:57:34 +0200

    New Updates of component players and tests on its classes

--- components/players.py
+++ components/players.py
@@ -1,28 +1,31 @@
-#   from components.table_player import TablePlayer
+from components.listings import players_positions
 
 
 class Players:
     """"""
-    preflop_starter = "BB"
-    postflop_starter = "BTN"
+    _preflop_starter = "BB"
+    _postflop_starter = "BTN"
+    _BB: int
+    pl_list: list
+    name_dict: dict
+    seat_dict: dict
+    positions_mapper: dict
 
     def __init__(self):
         self.pl_list = []
         self.name_dict = {}
         self.seat_dict = {}
-        self.positions = {}
 
     def __getitem__(self, item):
-        try:
-            if type(item) == str:
-                return self.name_dict[item]
-            elif type(item) == int:
-                return self.seat_dict[item]
-        except KeyError:
-            raise KeyError
+        if type(item) == str:
+            return self.name_dict[item]
+        elif type(item) == int:
+            return self.seat_dict[item]
+        else:
+            raise ValueError("To get a player, call it by its name or seat")
 
     def __len__(self):
-        return self.name_dict.__len__()
+        return len(self.occupied_seats)
 
     def __contains__(self, item):
         return self.pl_list.__contains__(item)
@@ -30,8 +33,39 @@ class Players:
     def __iter__(self):
         return self.pl_list.__iter__()
 
-    def find(self, name: str):
-        try:
-            return self.name_dict[name]
-        except KeyError:
-            print(f"{name} is not currently on this table")
+    @property
+    def len(self):
+        return self.__len__()
+
+    @property
+    def occupied_seats(self):
+        tab = list(self.seat_dict.keys())
+        tab.sort()
+        return tab
+
+    @property
+    def bb(self):
+        return self._BB
+
+    @bb.setter
+    def bb(self, seat):
+        if seat in self.occupied_seats:
+            self._BB = seat
+        else:
+            self._BB = self.occupied_seats[0]
+
+    @property
+    def ordered_seats(self):
+        cut = self.occupied_seats.index(self.bb) + 1
+        return self.occupied_seats[cut:] + self.occupied_seats[:cut]
+
+    @property
+    def positions_mapper(self):
+        nb_players = self.len
+        positions = players_positions[nb_players]
+        return dict(zip(self.ordered_seats, positions))
+
+    def distribute_positions(self):
+        for seat, pos in self.positions_mapper.items():
+            pl = self.seat_dict[seat]
+            pl.position = pos
