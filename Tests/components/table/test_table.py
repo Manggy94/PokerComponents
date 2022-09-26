c3764b72e57c0b2fc910b42ca110b2df7fe0ffe7
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-09-26 14:26:06 +0200

    New Updates of component table and tests on its classes

--- Tests/components/table/test_table.py
+++ Tests/components/table/test_table.py
@@ -1,9 +1,23 @@
 import unittest
 import numpy as np
 import components.table as table
+import components.tournament as tournament
+import components.table_player as player
+from components.constants import Street
 
 
 class MyTableTestCase(unittest.TestCase):
+
+    def setUp(self) -> None:
+        self.level = tournament.Level(4, 400)
+        self.level2 = tournament.Level(5, 600)
+        self.tour = tournament.Tournament(level=self.level)
+        self.p1 = player.TablePlayer(name="Toto", seat=1, stack=2000)
+        self.p2 = player.TablePlayer(name="Tata", seat=2, stack=2500)
+        self.p3 = player.TablePlayer(name="Titi", seat=6, stack=25000)
+        self.p4 = player.TablePlayer(name="Tété", seat=4, stack=120327)
+        self.p5 = player.TablePlayer(name="Tutu", seat=5, stack=267)
+        self.pl_list = [self.p1, self.p2, self.p3, self.p4]
 
     def test_new_table(self):
         tab = table.Table()
@@ -13,6 +27,8 @@ class MyTableTestCase(unittest.TestCase):
         self.assertIsInstance(tab._pots, list)
         self.assertIsInstance(tab.players, table.Players)
         self.assertIsInstance(tab.current_pot, table.Pot)
+        self.assertIsInstance(tab.street, Street)
+        self.assertEqual(tab.street, Street.PREFLOP)
         self.assertEqual(tab.pot, 0)
         self.assertEqual(tab.deck.len, 52)
         self.assertEqual(tab.board.len, 0)
@@ -21,6 +37,21 @@ class MyTableTestCase(unittest.TestCase):
             tab.max_players = 11
         tab.max_players = 9
         self.assertEqual(tab.max_players, 9)
+        tab.level = self.level2
+        self.assertEqual(tab.level.sb, 300)
+        tab.add_tournament(self.tour)
+        self.assertEqual(tab.level.sb, 200)
+        self.assertIsInstance(tab.tournament, tournament.Tournament)
+        self.assertIsInstance(tab._tournament, tournament.Tournament)
+        self.assertEqual(self.level.bb, 400)
+
+    def test_playing_order(self):
+        tab = table.Table()
+        for pl in self.pl_list:
+            pl.sit(tab)
+        self.assertEqual(tab.playing_order, [2, 4, 6, 1])
+        tab._street = Street.FLOP
+        self.assertEqual(tab.playing_order, [6, 1, 2, 4])
 
     def test_draws(self):
         tab = table.Table()
@@ -41,8 +72,17 @@ class MyTableTestCase(unittest.TestCase):
         self.assertEqual(tab.board["river"], "Jd")
         self.assertTrue((tab.board.values == np.array(["As", "Ad", "Ah", "Ac", "Jd"])).all())
 
-
-
+    def test_pregame_betting(self):
+        tab = table.Table()
+        tab.max_players = 6
+        tab.add_tournament(self.tour)
+        for pl in self.pl_list:
+            pl.sit(tab)
+        tab.bb = 2
+        tab.players.distribute_positions()
+        tab.post_pregame()
+        self.assertEqual(tab.current_pot.value, 800)
+        self.assertEqual(tab.current_pot.highest_bet, tab.level.bb)
 
 
 if __name__ == '__main__':
--- components/table.py
+++ components/table.py
@@ -1,7 +1,10 @@
 from components.board import Board
 from components.card import Deck
+from components.constants import Street
 from components.players import Players
 from components.pot import Pot
+from components.tournament import Level, Tournament
+from components.evaluator import Evaluator
 
 
 class Table:
@@ -11,6 +14,11 @@ class Table:
     _players: Players
     _pots: [Pot]
     _max_players: int
+    _is_mtt: bool
+    _tournament: Tournament
+    _level: Level
+    _street: Street
+    evaluator: Evaluator
 
     def __init__(self):
         self._board = Board()
@@ -18,6 +26,9 @@ class Table:
         self._deck.shuffle()
         self._players = Players()
         self._pots = [Pot()]
+        self.evaluator = Evaluator()
+        self._is_mtt = False
+        self._street = Street.PREFLOP
 
     @property
     def board(self):
@@ -47,8 +58,42 @@ class Table:
             self._max_players = max_value
 
     @property
-    def pot(self):
+    def pot(self) -> float:
         return sum((pot.value for pot in self._pots))
+
+    @property
+    def tournament(self):
+        return self._tournament
+
+    @tournament.setter
+    def tournament(self, tournament):
+        self._tournament = tournament
+
+    @property
+    def street(self):
+        return self._street
+
+    @street.setter
+    def street(self, street):
+        self._street = Street(street)
+
+    @property
+    def level(self):
+        if self._is_mtt:
+            return self._tournament.level
+        else:
+            return self._level
+
+    @level.setter
+    def level(self, level):
+        self._level = level
+
+    @property
+    def playing_order(self):
+        if self._street == Street.PREFLOP:
+            return self.players.preflop_ordered_seats
+        else:
+            return self.players.postflop_ordered_seats
 
     def draw_flop(self, c1=None, c2=None, c3=None):
         if len(self._board) > 0:
@@ -59,6 +104,7 @@ class Table:
         self._board.add(c1)
         self._board.add(c2)
         self._board.add(c3)
+        self.street = "flop"
         print(f"Flop: {c1}, {c2}, {c3}")
         print(f"Board: {[x for x in self._board[:3]]}")
 
@@ -67,6 +113,7 @@ class Table:
             raise ValueError("Board size must be 3 before we can draw a turn")
         card = self._deck.draw(card)
         self._board.add(card)
+        self.street = "turn"
         print(f"Turn: {card}")
         print(f"Board: {[x for x in self._board[:4]]}")
 
@@ -75,5 +122,32 @@ class Table:
             raise ValueError("Board size must be 4 before we can draw a turn")
         card = self._deck.draw(card)
         self._board.add(card)
+        self.street = "river"
         print(f"River: {card}")
         print(f"Board: {[x for x in self._board[:5]]}")
+
+    def add_tournament(self, tournament):
+        self.tournament = tournament
+        self._is_mtt = True
+
+    def post_antes(self):
+        for i in self.players.preflop_ordered_seats:
+            player = self.players.seat_dict[i]
+            player.post(self.level.ante)
+
+    def post_sb(self):
+        seat = self.players.seats_mapper["SB"]
+        player = self.players.seat_dict[seat]
+        if player.can_play:
+            player.bet(self.level.sb)
+
+    def post_bb(self):
+        seat = self.players.seats_mapper["BB"]
+        player = self.players.seat_dict[seat]
+        if player.can_play:
+            player.bet(self.level.bb)
+
+    def post_pregame(self):
+        self.post_antes()
+        self.post_sb()
+        self.post_bb()
