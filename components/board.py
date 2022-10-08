36756dfe3acf554166be728acf7600f2a2011190
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-10-08 13:52:11 +0200

    New Updates of component board and tests on its classes

--- components/board.py
+++ components/board.py
@@ -19,21 +19,36 @@ class Board(pd.Series):
 
     @property
     def len(self):
+        """
+        Returns the number of cards on the board
+        """
         return self.__len__()
 
     @property
     def flop(self):
+        """
+        Returns the cards drawn at flop
+        """
         return self.iloc[:3]
 
     @property
     def turn(self):
+        """
+        Returns the turn card
+        """
         return self.iloc[3]
 
     @property
     def river(self):
+        """
+        Returns the river card
+        """
         return self.iloc[4]
 
     def add(self, card):
+        """
+        Add a card to the board
+        """
         if len(self) == 5:
             raise ValueError("Board is already full with 5 cards")
         card = f"{Card(card)}"
@@ -43,6 +58,9 @@ class Board(pd.Series):
 
     @cached_property
     def flop_combinations(self):
+        """
+        Returns an array of all the combos from the flop
+        """
         try:
             return np.array([Combo.from_tuple(x) for x in combinations(self[:3], 2)])
         except TypeError:
@@ -50,6 +68,7 @@ class Board(pd.Series):
 
     @cached_property
     def is_rainbow(self):
+        """Boolean indicating if flop is rainbow"""
         if len(self) < 3:
             return None
         return all(
@@ -57,42 +76,59 @@ class Board(pd.Series):
         )
 
     def _get_differences(self):
+        """Returns a tuple of differences between flop cards"""
         if self.flop_combinations is None:
             return None
         return tuple(Rank.difference(combo.first.rank, combo.second.rank)for combo in self.flop_combinations)
 
     @cached_property
     def is_monotone(self):
+        """
+        Boolean indicating if flop is monotone
+        """
         if len(self) < 3:
             return None
         return all(combo.first.suit == combo.second.suit for combo in self.flop_combinations)
 
     @cached_property
     def is_triplet(self):
+        """Boolean indicating if flop is triplet"""
         if len(self) < 3:
             return None
         return all(diff == 0 for diff in self._get_differences())
 
     @cached_property
     def has_pair(self):
+        """
+        Boolean indicating if flop has a pair
+        """
         if len(self) < 3:
             return None
         return any(diff == 0 for diff in self._get_differences())
 
     @cached_property
     def has_straightdraw(self):
+        """
+        Boolean indicating if flop has a straightdraw
+        """
         if len(self) < 3:
             return None
         return any(1 <= diff <= 3 for diff in self._get_differences())
 
     @cached_property
     def has_gutshot(self):
+        """
+        Boolean indicating if flop has a gutshot
+        """
         if len(self) < 3:
             return None
         return any(1 <= diff <= 4 for diff in self._get_differences())
 
     @cached_property
     def has_flushdraw(self):
+        """
+        Boolean indicating if flop has a flushdraw
+        """
         if len(self) < 3:
             return None
         return any(combo.first.suit == combo.second.suit for combo in self.flop_combinations)
