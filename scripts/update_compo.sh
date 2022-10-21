112afcf9dbbd8f7490f15d3405dc60280defa577
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-10-21 23:10:19 +0200

    :sparkles: Update on scripts for fancy CI/CD

--- scripts/update_compo.sh
+++ scripts/update_compo.sh
@@ -6,7 +6,7 @@ function git_update_component()
     echo "Updating component $1"
     git add ./components/$1.py ./Tests/components/$1
     echo "Updates of component $1 and its tests are ready to be commited"
-    git commit -m "New Updates of component $1 and tests on its classes"
+    git commit -m ":sparkles: New Updates of component $1 and tests on its classes"
     git push
     
 }
