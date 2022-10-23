e1294d0e921ccf3b75dd9e7d89822b7e13d3461d
MANGWA Alexandre <alexandre.mangwa@soprasteria.com>
2022-10-23 17:02:20 +0200

    :sparkles: Modification into a Pypi package

---
+++ README.rst
@@ -0,0 +1,14 @@
+A Package of poker components for python
+========================================
+
+Description
+-----------
+A python package for coding around poker.
+Made to create poker related objects
+
+License
+-------
+
+The MIT License (MIT)
+
+Copyright (c) 2021-2022 Alexandre MANGWA
---
+++ setup.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+from setuptools import setup, find_packages
+
+
+install_requires = [        
+    "parsedatetime",
+    "cached-property",
+    "numpy",
+    "pandas",
+    "pytest",
+    "coverage",
+    "pytest-cov"
+]
+
+classifiers = [
+    "Development Status :: 4 - Beta",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Natural Language :: English",
+    "Natural Language :: French",
+    "Programming Language :: Python",
+    "Programming Language :: Python :: 3",
+    "Topic :: Games/Entertainment",
+    "Topic :: Games/Entertainment :: Board Games"
+]
+
+setup(
+    name="poker-components",
+    version="0.0.1",
+    description="A Poker Package",
+    long_description=Path("README.rst").read_text(),
+    classifiers=classifiers,
+    keywords="poker package",
+    author="Alexandre MANGWA",
+    author_email="alex.mangwa@gmail.com",
+    url="https://github.com/manggy94/PokerComponents",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=install_requires,
+    tests_require=["pytest", "pytest-cov", "coverage", "coveralls"],
+)
